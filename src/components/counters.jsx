0630fc98a5555b535994dd7259aaba3ffbb8edfa
Lucas Wu <lucaswuhuang@gmail.com>
2022-07-31 00:13:44 -0700

    Configure React app for deployment to GitHub Pages

---
+++ src/components/counters.jsx
@@ -0,0 +1,28 @@
+// 1:47
+
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-small m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
